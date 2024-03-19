38dfc7df90837eb73d19ad72a1a1ceac767a39de
James King <151979414+jamesking020773@users.noreply.github.com>
2024-03-19 15:11:22 +1100

    Update views.py

--- at1/eduprod/views.py
+++ at1/eduprod/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import Question
 
 def index(request):
-    questions = Question.objects.all()
+    questions = Question.objects.order_by('?')[:5]
+    # questions = Question.objects.all()
     questions_json = serializers.serialize('json', questions)
     return render(request, 'eduprod/index.html', {'questions_json': questions_json})
